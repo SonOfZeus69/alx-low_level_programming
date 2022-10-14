e337c5bd445341ca07bb2ae2b27355c39bcc3dbe
Babajide <sobowaleheyzeez@gmail.com>
2022-10-14 14:12:22 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: Always Return 0 (Success)
+ *
+ */
+
+int main(void)
+{
+char i = 'a';
+
+while (i <= 'z')
+{
+	putchar(i);
+	i++;
+}
+
+	putchar('\n');
+	return (0);
+}
