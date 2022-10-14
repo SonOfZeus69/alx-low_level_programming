586564892b08a6669c2178d26346e9320a1bbfbb
Babajide <sobowaleheyzeez@gmail.com>
2022-10-13 17:40:19 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,10 @@
 
 /**
  * main - write to stdout
- * Return: Always 0   
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
