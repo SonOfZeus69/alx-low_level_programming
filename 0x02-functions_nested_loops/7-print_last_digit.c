59f234312ffbbeb00670e02e7dca879ceecd015a
Babajide <sobowaleheyzeez@gmail.com>
2022-10-18 14:09:15 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of the inputted number
+ * @c: letter being tested with
+ * Return: Always return 0 (Success)
+ */
+
+int print_last_digit(int c)
+{
+return (c % 10);
+}
