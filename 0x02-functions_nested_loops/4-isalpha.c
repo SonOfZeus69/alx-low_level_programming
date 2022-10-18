8d832322f3e0d437683e15d1baab5ed43e198d51
Babajide <sobowaleheyzeez@gmail.com>
2022-10-18 11:04:08 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if the inputted character is in upppercase
+ * @c: the letter being used to test
+ * Return: Always return 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+if (c >= 65 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
