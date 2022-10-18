7cb3dc1c910a341bea56dad4987c099fae39a142
Babajide <sobowaleheyzeez@gmail.com>
2022-10-18 09:59:49 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+return 0 (Success);
+}
