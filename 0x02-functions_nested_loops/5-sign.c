aa21adfa29de24de5607b91b8d75b4734eb963fb
Babajide <sobowaleheyzeez@gmail.com>
2022-10-18 11:51:05 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an inputted number
+ * @n: characted being used to test
+ * Return: Always return 0 (Success)
+ */
+
+int print_sign(int n)
+{
+if (n == 0)
+{
+_putchar(n + '0');
+return (0);
+}
+else if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+else
+{
+_putchar("-");
+return (-1);
+}
+}
