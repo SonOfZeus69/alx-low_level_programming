fd0c371863a9912196c1e1c3e5433a38b111b787
Babajide <sobowaleheyzeez@gmail.com>
2022-10-21 16:58:55 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,6 +14,7 @@ _putchar('\n');
 else
 {
 int i, j;
+
 for (i = 1; i <= size; i++)
 {
 for (j = i; j < size; j++)
@@ -24,6 +25,7 @@ for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
+
 _putchar('\n');
 }
 }
