965f2ae9c281fe54b9bfb83ccab16790e46eaf78
Babajide <sobowaleheyzeez@gmail.com>
2022-11-18 09:36:14 -0800

    Fixed Tssk 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,16 +14,16 @@ int (*get_op_func(char *s))(int, int)
 	{ "+", op_add },
 	{ "-", op_sub },
 	{ "*", op_mul },
-	{ "/", op_div }
+	{ "/", op_div },
 	{ "%", op_mod },
-	{ NULL, NULL }
+	{ NULL, NULL },
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
