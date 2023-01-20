40731e350ae6c2f0d5567985cb334984dae27d6f
Babajide <sobowaleheyzeez@gmail.com>
2023-01-20 03:58:42 -0800

    task 0

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of the inputted number
+ * @n: letter being used to test
+ * Return: Always return 0 (Success)
+ */
+
+int _abs(int n)
+{
+if (n < 1)
+{
+return (n * -1);
+}
+return (n);
+}
